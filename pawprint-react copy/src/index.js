ff283f7246865d2c9a6fa41097338f9731d88a84
CookyZL <93364276+CookyZL@users.noreply.github.com>
2021-11-18 17:38:01 -0800

    merge map and login in one page

--- pawprint-react copy/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Button from "./components/button";
-import Map from "./components/map";
-import Update from "./components/update";
-ReactDOM.render(<Map />, document.getElementById("root"));
