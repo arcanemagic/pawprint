37c39114fff5ef1420987cf06b361b2d352dc7b6
jessy413 <jessy413@ucla.edu>
2021-11-24 11:56:54 -0800

    changed nav bar UI

--- login_register_files/client/src/components/Navigation.jsx
+++ login_register_files/client/src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../css/nav.css";
 import logo from "../images/pawprint_logo.png";
 function Navigation() {
   return (
@@ -7,14 +8,16 @@ function Navigation() {
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-            <img src={logo}></img>
+            <div className="pawprint_header">
+              <img className="pawprint_logo" src={logo} alt="Pawprint logo" />
+              <strong>Pawprint</strong>
+            </div>
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
                   Home
-                  <span className="sr-only">(current)</span>
                 </NavLink>
               </li>
               <li className="nav-item">
--- login_register_files/client/src/components/update.jsx
+++ login_register_files/client/src/components/update.jsx
@@ -16,65 +16,67 @@ class Update extends React.Component {
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
-        <div className="container">
+        <div className="update">
           <h1>Click where you saw him!</h1>
-          <img style={{ maxWidth: "1000px" }} src={map} alt="image" />
-          <div className="one" onClick={() => this.changeMap(1)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="two" onClick={() => this.changeMap(2)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="three" onClick={() => this.changeMap(3)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="four" onClick={() => this.changeMap(4)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="five" onClick={() => this.changeMap(5)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="six" onClick={() => this.changeMap(6)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="seven" onClick={() => this.changeMap(7)}>
-            <i className="material-icons">location_on</i>
-          </div>
+          <div>
+            <img style={{ maxWidth: "1000px" }} src={map} alt="image" />
+            <div className="one" onClick={() => this.changeMap(1)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="two" onClick={() => this.changeMap(2)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="three" onClick={() => this.changeMap(3)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="four" onClick={() => this.changeMap(4)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="five" onClick={() => this.changeMap(5)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="six" onClick={() => this.changeMap(6)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="seven" onClick={() => this.changeMap(7)}>
+              <i className="material-icons">location_on</i>
+            </div>
 
-          <div className="eight" onClick={() => this.changeMap(8)}>
-            <i className="material-icons">location_on</i>
-          </div>
+            <div className="eight" onClick={() => this.changeMap(8)}>
+              <i className="material-icons">location_on</i>
+            </div>
 
-          <div className="nine" onClick={() => this.changeMap(9)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="ten" onClick={() => this.changeMap(10)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="eleven" onClick={() => this.changeMap(11)}>
-            <i className="material-icons">location_on</i>
-          </div>
+            <div className="nine" onClick={() => this.changeMap(9)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="ten" onClick={() => this.changeMap(10)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="eleven" onClick={() => this.changeMap(11)}>
+              <i className="material-icons">location_on</i>
+            </div>
 
-          <div className="twelve" onClick={() => this.changeMap(12)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="thirteen" onClick={() => this.changeMap(13)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="fourteen" onClick={() => this.changeMap(14)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="fifteen" onClick={() => this.changeMap(15)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="sixteen" onClick={() => this.changeMap(16)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="seventeen" onClick={() => this.changeMap(17)}>
-            <i className="material-icons">location_on</i>
-          </div>
-          <div className="eighteen" onClick={() => this.changeMap(18)}>
-            <i className="material-icons">location_on</i>
+            <div className="twelve" onClick={() => this.changeMap(12)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="thirteen" onClick={() => this.changeMap(13)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="fourteen" onClick={() => this.changeMap(14)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="fifteen" onClick={() => this.changeMap(15)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="sixteen" onClick={() => this.changeMap(16)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="seventeen" onClick={() => this.changeMap(17)}>
+              <i className="material-icons">location_on</i>
+            </div>
+            <div className="eighteen" onClick={() => this.changeMap(18)}>
+              <i className="material-icons">location_on</i>
+            </div>
           </div>
         </div>
       </div>
