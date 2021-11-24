b65cf04f4d7763eaca95663c456513214a9f9b8d
Siyu Chen <schen1923@ucla.edu>
2021-11-24 11:04:18 -0800

    implemented update map feature
    upon clicking the update button, the most recent location of powell cat is recorded in the database and the home page image updates accordingly

---
+++ project/client/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Button from "./components/button";
+import Map from "./components/map";
+import Update from "./components/update";
+ReactDOM.render(<Map />, document.getElementById("root"));
