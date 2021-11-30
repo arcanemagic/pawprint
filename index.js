3e97fb96670b14d7a7970d262b165f0797c9e8be
Siyu Chen <schen1923@ucla.edu>
2021-11-30 04:02:52 -0800

    handled disconnection

--- index.js
+++ index.js
@@ -18,6 +18,36 @@ const db = mysql.createConnection({
     database: "heroku_ed6443bb67dd7cb",
 });
 
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    user: "badeb00fe5be21",
+    host: "us-cdbr-east-04.cleardb.com",
+    password: "e0d840f6",
+    database: "heroku_ed6443bb67dd7cb",
+}); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 app.post("/create", (req, res) => {
     const name = req.body.name;
     const username = req.body.username;
