74521cbec0f834816fb390e92ea98b383c6c6517
Siyu Chen <schen1923@ucla.edu>
2021-11-30 14:44:31 -0800

    handled timeout

--- index.js
+++ index.js
@@ -6,33 +6,41 @@ app.use(cors());
 app.use(express.json());
 const mysql = require('mysql');
 
+var db_config = {
+  host: 'us-cdbr-east-04.cleardb.com',
+    user: 'badeb00fe5be21',
+    password: 'e0d840f6',
+    database: 'heroku_ed6443bb67dd7cb'
+};
+
 // const host = '0.0.0.0';
 // const port = process.env.PORT || 8000;
 
 // mysql://badeb00fe5be21:e0d840f6@us-cdbr-east-04.cleardb.com/heroku_ed6443bb67dd7cb?reconnect=true
 
-const db = mysql.createConnection({
-    user: "badeb00fe5be21",
-    host: "us-cdbr-east-04.cleardb.com",
-    password: "e0d840f6",
-    database: "heroku_ed6443bb67dd7cb",
-});
+const db = mysql.createConnection(db_config);
 
-db.connect(function(err) {              // The server is either down
-  if(err) {                                     // or restarting (takes a while sometimes).
-    console.log('error when connecting to db:', err);
-    setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-  }                                     // to avoid a hot loop, and to allow our node script to
-});                                     // process asynchronous requests in the meantime.
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, sincethe old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-db.on('error', function(err) {
-  console.log('db error', err);
-  if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-    handleDisconnect();                         // lost due to either server restart, or a
-  } else {                                      // connnection idle timeout (the wait_timeout
-    throw err;                                  // server variable configures this)
-  }
-});
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.post("/create", (req, res) => {
     const name = req.body.name;
