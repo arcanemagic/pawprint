0afb223d703e9a72a7bb25d0ba68d420143cdca0
AR2302 <adithi.ramesh02@gmail.com>
2021-11-30 23:31:59 -0800

    Post, Trending, Image Upload CSS
    - Improved the CSS for trending and post

--- main/client/src/components/Trending.jsx
+++ main/client/src/components/Trending.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState } from "react";
 import "../css/Post.css";
-import username_logo from "../images/social/username_logo.png";
+import username_logo from "../images/social/cute_dog.jpg";
 import comment_symbol from "../images/social/comment_symbol.png";
 import nolikes from "../images/social/powell_cat_nolikes.png";
 import likes from "../images/social/powell_cat_likes.png";
@@ -23,29 +23,34 @@ function Icon(props) {
   
     return (
       <div>
-         <div>
-         <h2 className="pp">Trending posts: </h2>
         {trends.map((val, key) => {
           return (
             <div className="user_post">
+              <div className="username_header">
+             <img
+                className="username_logo"
+                src={username_logo}
+                alt="logo"/>
+              <strong>{val.user_id}</strong>
+            </div>
               <div className="post_image">
                 <Image cloudName="bruin-pawprint" publicId={val.image} />
               </div>
               <div>
-                <div className="user_caption">
-                  {" "}
-                  {"Description:     "}{val.title}
-                </div>
               </div>
               <div className="likes"> 
                 <Icon symbol = {likes} altName = "likes icon"/>
                 {val.num_like} likes </div>
+                <div className="user_caption">
+                {" "}
+                <strong>{val.user_id}</strong> {" "} {val.title}
+              </div>
             </div>
           );
         })}
       </div>
-      </div>
     );
-  }
+}
+
   export default Trending;
   
